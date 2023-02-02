6ea08164fb4268446fa25c5b6b163225c2453da8
kaganunal <107484578+kaganunal@users.noreply.github.com>
2023-02-02 18:51:09 +0300

    Add files via upload

---
+++ kullaniciAdiSifre/kullaniciAdiSifre/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace kullaniciAdiSifre
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblKullaniciAdi = new System.Windows.Forms.Label();
+            this.lblSifre = new System.Windows.Forms.Label();
+            this.txtKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.checkBoxGoster = new System.Windows.Forms.CheckBox();
+            this.btnGiris = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblKullaniciAdi
+            // 
+            this.lblKullaniciAdi.AutoSize = true;
+            this.lblKullaniciAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKullaniciAdi.Location = new System.Drawing.Point(201, 93);
+            this.lblKullaniciAdi.Name = "lblKullaniciAdi";
+            this.lblKullaniciAdi.Size = new System.Drawing.Size(132, 25);
+            this.lblKullaniciAdi.TabIndex = 0;
+            this.lblKullaniciAdi.Text = "Kullanıcı Adı";
+            this.lblKullaniciAdi.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblSifre
+            // 
+            this.lblSifre.AutoSize = true;
+            this.lblSifre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSifre.Location = new System.Drawing.Point(276, 154);
+            this.lblSifre.Name = "lblSifre";
+            this.lblSifre.Size = new System.Drawing.Size(57, 25);
+            this.lblSifre.TabIndex = 2;
+            this.lblSifre.Text = "Şifre";
+            // 
+            // txtKullaniciAdi
+            // 
+            this.txtKullaniciAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtKullaniciAdi.Location = new System.Drawing.Point(352, 93);
+            this.txtKullaniciAdi.Multiline = true;
+            this.txtKullaniciAdi.Name = "txtKullaniciAdi";
+            this.txtKullaniciAdi.Size = new System.Drawing.Size(152, 25);
+            this.txtKullaniciAdi.TabIndex = 1;
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSifre.Location = new System.Drawing.Point(352, 154);
+            this.txtSifre.Multiline = true;
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(152, 25);
+            this.txtSifre.TabIndex = 3;
+            // 
+            // checkBoxGoster
+            // 
+            this.checkBoxGoster.AutoSize = true;
+            this.checkBoxGoster.Location = new System.Drawing.Point(510, 159);
+            this.checkBoxGoster.Name = "checkBoxGoster";
+            this.checkBoxGoster.Size = new System.Drawing.Size(69, 20);
+            this.checkBoxGoster.TabIndex = 4;
+            this.checkBoxGoster.Text = "Göster";
+            this.checkBoxGoster.UseVisualStyleBackColor = true;
+            this.checkBoxGoster.CheckedChanged += new System.EventHandler(this.checkBoxGoster_CheckedChanged);
+            // 
+            // btnGiris
+            // 
+            this.btnGiris.Location = new System.Drawing.Point(352, 197);
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.Size = new System.Drawing.Size(152, 41);
+            this.btnGiris.TabIndex = 5;
+            this.btnGiris.Text = "Giriş";
+            this.btnGiris.UseVisualStyleBackColor = true;
+            this.btnGiris.Click += new System.EventHandler(this.btnGiris_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGiris);
+            this.Controls.Add(this.checkBoxGoster);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.txtKullaniciAdi);
+            this.Controls.Add(this.lblSifre);
+            this.Controls.Add(this.lblKullaniciAdi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblKullaniciAdi;
+        private System.Windows.Forms.Label lblSifre;
+        private System.Windows.Forms.TextBox txtKullaniciAdi;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.CheckBox checkBoxGoster;
+        private System.Windows.Forms.Button btnGiris;
+    }
+}
+
---
+++ kullaniciAdiSifre/kullaniciAdiSifre/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kullaniciAdiSifre
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtSifre.UseSystemPasswordChar = true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGiris_Click(object sender, EventArgs e)
+        {
+
+            if (txtSifre.Text == "1234" && txtKullaniciAdi.Text == "bilge.adam") 
+            {
+                MessageBox.Show("Giriş başarılı");  
+            }
+            else
+            {
+                MessageBox.Show("Tekrardan giriş yapınız");
+            }
+        }
+
+        private void checkBoxGoster_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (checkBoxGoster.Checked)
+            {
+                txtSifre.UseSystemPasswordChar = false;
+                checkBoxGoster.Text = "Gizle";
+            }
+            else 
+            {
+                txtSifre.UseSystemPasswordChar = true;
+                checkBoxGoster.Text = "Göster";
+            }
+        }
+    }
+}
