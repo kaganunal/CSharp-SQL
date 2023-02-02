400e5214b018cfa16f12c39286d3fda0f97c7c18
Oguz Kagan Unal <107484578+kaganunal@users.noreply.github.com>
2023-02-02 21:42:54 +0300

    Personel Bilgisi Ekleme (İsim-Soyisim-Şehir)

--- personelEkleme/personelEkleme/Form1.Designer.cs
+++ personelEkleme/personelEkleme/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.lblAd = new System.Windows.Forms.Label();
             this.lblSoyad = new System.Windows.Forms.Label();
             this.lblSehir = new System.Windows.Forms.Label();
@@ -44,7 +45,7 @@
             // 
             this.lblAd.AutoSize = true;
             this.lblAd.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblAd.Location = new System.Drawing.Point(86, 69);
+            this.lblAd.Location = new System.Drawing.Point(131, 65);
             this.lblAd.Name = "lblAd";
             this.lblAd.Size = new System.Drawing.Size(33, 23);
             this.lblAd.TabIndex = 0;
@@ -54,7 +55,7 @@
             // 
             this.lblSoyad.AutoSize = true;
             this.lblSoyad.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblSoyad.Location = new System.Drawing.Point(86, 109);
+            this.lblSoyad.Location = new System.Drawing.Point(104, 112);
             this.lblSoyad.Name = "lblSoyad";
             this.lblSoyad.Size = new System.Drawing.Size(60, 23);
             this.lblSoyad.TabIndex = 0;
@@ -64,7 +65,7 @@
             // 
             this.lblSehir.AutoSize = true;
             this.lblSehir.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblSehir.Location = new System.Drawing.Point(86, 160);
+            this.lblSehir.Location = new System.Drawing.Point(113, 156);
             this.lblSehir.Name = "lblSehir";
             this.lblSehir.Size = new System.Drawing.Size(51, 23);
             this.lblSehir.TabIndex = 0;
@@ -72,22 +73,26 @@
             // 
             // listIsim
             // 
+            this.listIsim.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.listIsim.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.listIsim.FormattingEnabled = true;
             this.listIsim.ItemHeight = 23;
-            this.listIsim.Location = new System.Drawing.Point(375, 62);
+            this.listIsim.Location = new System.Drawing.Point(433, 62);
+            this.listIsim.Margin = new System.Windows.Forms.Padding(10);
+            this.listIsim.MultiColumn = true;
             this.listIsim.Name = "listIsim";
-            this.listIsim.Size = new System.Drawing.Size(323, 303);
-            this.listIsim.TabIndex = 1;
+            this.listIsim.Size = new System.Drawing.Size(298, 303);
+            this.listIsim.TabIndex = 0;
             // 
             // btnEkle
             // 
             this.btnEkle.BackColor = System.Drawing.Color.Lime;
             this.btnEkle.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btnEkle.Location = new System.Drawing.Point(68, 228);
+            this.btnEkle.Margin = new System.Windows.Forms.Padding(10);
             this.btnEkle.Name = "btnEkle";
             this.btnEkle.Size = new System.Drawing.Size(234, 73);
-            this.btnEkle.TabIndex = 2;
+            this.btnEkle.TabIndex = 9;
             this.btnEkle.Text = "EKLE";
             this.btnEkle.UseVisualStyleBackColor = false;
             this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
@@ -97,44 +102,53 @@
             this.btnGuncelle.BackColor = System.Drawing.Color.Orange;
             this.btnGuncelle.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btnGuncelle.Location = new System.Drawing.Point(188, 307);
+            this.btnGuncelle.Margin = new System.Windows.Forms.Padding(10);
             this.btnGuncelle.Name = "btnGuncelle";
             this.btnGuncelle.Size = new System.Drawing.Size(114, 63);
-            this.btnGuncelle.TabIndex = 2;
+            this.btnGuncelle.TabIndex = 8;
             this.btnGuncelle.Text = "GÜNCELLE";
             this.btnGuncelle.UseVisualStyleBackColor = false;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
             // 
             // btnSil
             // 
             this.btnSil.BackColor = System.Drawing.Color.Red;
             this.btnSil.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btnSil.Location = new System.Drawing.Point(68, 307);
+            this.btnSil.Margin = new System.Windows.Forms.Padding(10);
             this.btnSil.Name = "btnSil";
             this.btnSil.Size = new System.Drawing.Size(114, 63);
-            this.btnSil.TabIndex = 2;
+            this.btnSil.TabIndex = 5;
             this.btnSil.Text = "SİL";
             this.btnSil.UseVisualStyleBackColor = false;
             this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
             // 
             // txtAd
             // 
+            this.txtAd.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.txtAd.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.txtAd.Location = new System.Drawing.Point(170, 62);
+            this.txtAd.Margin = new System.Windows.Forms.Padding(10);
             this.txtAd.Name = "txtAd";
             this.txtAd.Size = new System.Drawing.Size(132, 30);
-            this.txtAd.TabIndex = 3;
+            this.txtAd.TabIndex = 1;
             // 
             // txtSoyad
             // 
+            this.txtSoyad.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.txtSoyad.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.txtSoyad.Location = new System.Drawing.Point(170, 109);
+            this.txtSoyad.Margin = new System.Windows.Forms.Padding(10);
             this.txtSoyad.Name = "txtSoyad";
             this.txtSoyad.Size = new System.Drawing.Size(132, 30);
-            this.txtSoyad.TabIndex = 3;
+            this.txtSoyad.TabIndex = 2;
             // 
             // txtSehir
             // 
+            this.txtSehir.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.txtSehir.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.txtSehir.Location = new System.Drawing.Point(170, 157);
+            this.txtSehir.Location = new System.Drawing.Point(170, 153);
+            this.txtSehir.Margin = new System.Windows.Forms.Padding(10);
             this.txtSehir.Name = "txtSehir";
             this.txtSehir.Size = new System.Drawing.Size(132, 30);
             this.txtSehir.TabIndex = 3;
@@ -143,7 +157,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(803, 441);
             this.Controls.Add(this.txtSehir);
             this.Controls.Add(this.txtSoyad);
             this.Controls.Add(this.txtAd);
--- personelEkleme/personelEkleme/Form1.cs
+++ personelEkleme/personelEkleme/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,11 +17,34 @@ namespace personelEkleme
         public Form1()
         {
             InitializeComponent();
+
+            string ad = txtAd.Text.Trim();
+            string soyad = txtSoyad.Text.Trim();
+            string sehir = txtSehir.Text.Trim();
+
+            TextInfo myTI = new CultureInfo("en-US", false).TextInfo;
+
+            ad = myTI.ToTitleCase(ad);
+            sehir = myTI.ToTitleCase(sehir);
+            soyad = myTI.ToUpper(soyad);
+
         }
 
         private void btnEkle_Click(object sender, EventArgs e)
         {
-            listIsim.Items.Add(txtAd.Text + " " + txtSoyad.Text + " | " + txtSehir.Text);
+            string ad = txtAd.Text;
+            string soyad = txtSoyad.Text;
+            string sehir = txtSehir.Text;
+
+            TextInfo myTI = new CultureInfo("en-US", false).TextInfo;
+
+            ad = myTI.ToTitleCase(ad);
+            sehir = myTI.ToTitleCase(sehir);
+            soyad = myTI.ToUpper(soyad);
+
+            //listIsim.Items.Add(ad.Substring(0, 1).ToUpper() + txtAd.Text.Substring(1).ToLower() + " " + txtSoyad.Text + " | " + txtSehir.Text.Substring(0, 1).ToUpper() + txtSehir.Text.Substring(1).ToLower());
+
+            listIsim.Items.Add(ad + " " + soyad + " | " + sehir);
             txtAd.Clear();
             txtSoyad.Clear();
             txtSehir.Clear();
@@ -28,7 +52,39 @@ namespace personelEkleme
 
         private void btnSil_Click(object sender, EventArgs e)
         {
-            listIsim.Items.Remove(listIsim.SelectedItem);
+            if (listIsim.SelectedIndex == -1)
+            {
+                MessageBox.Show("Lütfen silinecek bir veri seçiniz!");
+            }
+            else
+            {
+                listIsim.Items.Remove(listIsim.SelectedItem);
+            }
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            string ad = txtAd.Text.Trim();
+            string soyad = txtSoyad.Text.Trim();
+            string sehir = txtSehir.Text.Trim();
+
+            TextInfo myTI = new CultureInfo("en-US", false).TextInfo;
+
+            ad = myTI.ToTitleCase(ad);
+            sehir = myTI.ToTitleCase(sehir);
+            soyad = myTI.ToUpper(soyad);
+
+            if (listIsim.SelectedIndex == -1)
+            {
+                MessageBox.Show("Lütfen güncellenecek bir veri seçiniz!");
+            }
+            else
+            {
+                int index = listIsim.SelectedIndex;
+                listIsim.Items.Remove(listIsim.SelectedItem);
+                listIsim.Items.Insert(index, ad + " " + soyad + " | " + sehir);
+            }
+
         }
     }
 }
