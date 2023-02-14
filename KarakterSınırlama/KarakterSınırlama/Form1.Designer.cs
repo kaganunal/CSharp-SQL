db633b3a003b2860954cb1d7688fe667e8ea0528
Oguz Kagan Unal <107484578+kaganunal@users.noreply.github.com>
2023-02-14 17:01:57 +0300

    255 karakterle sınırlama

---
+++ KarakterSınırlama/KarakterSınırlama/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace KarakterSınırlama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSinir = new System.Windows.Forms.Label();
+            this.rchText = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // lblSinir
+            // 
+            this.lblSinir.BackColor = System.Drawing.SystemColors.Menu;
+            this.lblSinir.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblSinir.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSinir.ForeColor = System.Drawing.Color.Black;
+            this.lblSinir.Location = new System.Drawing.Point(0, 357);
+            this.lblSinir.Name = "lblSinir";
+            this.lblSinir.Size = new System.Drawing.Size(800, 93);
+            this.lblSinir.TabIndex = 0;
+            this.lblSinir.Text = "Kalan Karakter Sayısı:";
+            this.lblSinir.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // rchText
+            // 
+            this.rchText.Location = new System.Drawing.Point(12, 12);
+            this.rchText.Name = "rchText";
+            this.rchText.Size = new System.Drawing.Size(776, 327);
+            this.rchText.TabIndex = 1;
+            this.rchText.Text = "";
+            this.rchText.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rchText);
+            this.Controls.Add(this.lblSinir);
+            this.ForeColor = System.Drawing.Color.RosyBrown;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSinir;
+        private System.Windows.Forms.RichTextBox rchText;
+    }
+}
+
---
+++ KarakterSınırlama/KarakterSınırlama/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace KarakterSınırlama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            int sinir = rchText.TextLength;
+            lblSinir.Text = "Kalan Karakter Sayısı: " + (255 - rchText.TextLength).ToString();
+            if (sinir == 255)
+            {
+                MessageBox.Show("Maksimum sınıra ulaştınız!");
+            }
+
+            if (sinir <= 50)
+            {
+                lblSinir.ForeColor = Color.Black;
+            }
+            if (sinir <= 100 && sinir > 50)
+            {
+                lblSinir.ForeColor = Color.DarkRed;
+            }
+            if (sinir <= 150 && sinir > 100 )
+            {
+                lblSinir.ForeColor = Color.Firebrick;
+            }
+            if (sinir <= 200 && sinir > 150)
+            {
+                lblSinir.ForeColor = Color.Crimson;
+            }
+            if (sinir <= 255 && sinir >200)
+            {
+                lblSinir.ForeColor = Color.Red;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rchText.MaxLength = 255;
+        }
+
+
+    }
+}
