ec5c73618ea3832c38caaa1759ba106d2407728b
Oguz Kagan Unal <107484578+kaganunal@users.noreply.github.com>
2023-02-14 17:03:25 +0300

    Kullanıcı adı şifre ve yeni formda hoşgeldin

---
+++ KullaniciAdiSifreHosgeldin/KullaniciAdiSifreHosgeldin/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace KullaniciAdiSifreHosgeldin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblKullanicAdi = new System.Windows.Forms.Label();
+            this.txtKullanici = new System.Windows.Forms.TextBox();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.lblSifre = new System.Windows.Forms.Label();
+            this.btnGiris = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblKullanicAdi
+            // 
+            this.lblKullanicAdi.AutoSize = true;
+            this.lblKullanicAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKullanicAdi.Location = new System.Drawing.Point(98, 83);
+            this.lblKullanicAdi.Name = "lblKullanicAdi";
+            this.lblKullanicAdi.Size = new System.Drawing.Size(125, 25);
+            this.lblKullanicAdi.TabIndex = 0;
+            this.lblKullanicAdi.Text = "Kullanıcı Adı:";
+            // 
+            // txtKullanici
+            // 
+            this.txtKullanici.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtKullanici.Location = new System.Drawing.Point(256, 83);
+            this.txtKullanici.Name = "txtKullanici";
+            this.txtKullanici.Size = new System.Drawing.Size(175, 30);
+            this.txtKullanici.TabIndex = 1;
+
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSifre.Location = new System.Drawing.Point(256, 123);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(175, 30);
+            this.txtSifre.TabIndex = 3;
+            // 
+            // lblSifre
+            // 
+            this.lblSifre.AutoSize = true;
+            this.lblSifre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSifre.Location = new System.Drawing.Point(98, 126);
+            this.lblSifre.Name = "lblSifre";
+            this.lblSifre.Size = new System.Drawing.Size(58, 25);
+            this.lblSifre.TabIndex = 2;
+            this.lblSifre.Text = "Şifre:";
+            // 
+            // btnGiris
+            // 
+            this.btnGiris.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGiris.Location = new System.Drawing.Point(179, 175);
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.Size = new System.Drawing.Size(152, 49);
+            this.btnGiris.TabIndex = 4;
+            this.btnGiris.Text = "Giriş Yap";
+            this.btnGiris.UseVisualStyleBackColor = true;
+            this.btnGiris.Click += new System.EventHandler(this.btnGiris_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 286);
+            this.Controls.Add(this.btnGiris);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.lblSifre);
+            this.Controls.Add(this.txtKullanici);
+            this.Controls.Add(this.lblKullanicAdi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblKullanicAdi;
+        private System.Windows.Forms.TextBox txtKullanici;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.Label lblSifre;
+        private System.Windows.Forms.Button btnGiris;
+    }
+}
+
