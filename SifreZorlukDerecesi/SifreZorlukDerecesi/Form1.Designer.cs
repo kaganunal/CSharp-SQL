0fdcefab510357e67e466b0179ba4c6527129073
Oguz Kagan Unal <107484578+kaganunal@users.noreply.github.com>
2023-02-14 17:00:59 +0300

    Şifre Zorluk Derecesi

---
+++ SifreZorlukDerecesi/SifreZorlukDerecesi/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace SifreZorlukDerecesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.lblYazi = new System.Windows.Forms.Label();
+            this.lblZorluk = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblKullanici = new System.Windows.Forms.Label();
+            this.txtKullanici = new System.Windows.Forms.TextBox();
+            this.btnGiris = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSifreTekrar = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSifre.Location = new System.Drawing.Point(49, 145);
+            this.txtSifre.Multiline = true;
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(332, 26);
+            this.txtSifre.TabIndex = 1;
+            this.txtSifre.TextChanged += new System.EventHandler(this.txtSifre_TextChanged);
+            // 
+            // lblYazi
+            // 
+            this.lblYazi.AutoSize = true;
+            this.lblYazi.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblYazi.Location = new System.Drawing.Point(54, 185);
+            this.lblYazi.Name = "lblYazi";
+            this.lblYazi.Size = new System.Drawing.Size(151, 21);
+            this.lblYazi.TabIndex = 1;
+            this.lblYazi.Text = "Şifre Zorluk Derecesi";
+            // 
+            // lblZorluk
+            // 
+            this.lblZorluk.AutoSize = true;
+            this.lblZorluk.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblZorluk.ForeColor = System.Drawing.Color.Red;
+            this.lblZorluk.Location = new System.Drawing.Point(239, 185);
+            this.lblZorluk.Name = "lblZorluk";
+            this.lblZorluk.Size = new System.Drawing.Size(53, 21);
+            this.lblZorluk.TabIndex = 2;
+            this.lblZorluk.Text = "Düşük";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(54, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Şifre:";
+            // 
+            // lblKullanici
+            // 
+            this.lblKullanici.AutoSize = true;
+            this.lblKullanici.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKullanici.Location = new System.Drawing.Point(54, 44);
+            this.lblKullanici.Name = "lblKullanici";
+            this.lblKullanici.Size = new System.Drawing.Size(118, 24);
+            this.lblKullanici.TabIndex = 5;
+            this.lblKullanici.Text = "Kullanıcı Adı:";
+            // 
+            // txtKullanici
+            // 
+            this.txtKullanici.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtKullanici.Location = new System.Drawing.Point(49, 71);
+            this.txtKullanici.Multiline = true;
+            this.txtKullanici.Name = "txtKullanici";
+            this.txtKullanici.Size = new System.Drawing.Size(332, 26);
+            this.txtKullanici.TabIndex = 0;
+            // 
+            // btnGiris
+            // 
+            this.btnGiris.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnGiris.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGiris.Location = new System.Drawing.Point(103, 297);
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.Size = new System.Drawing.Size(209, 44);
+            this.btnGiris.TabIndex = 3;
+            this.btnGiris.Text = "Hesap Oluştur";
+            this.btnGiris.UseVisualStyleBackColor = false;
+            this.btnGiris.Click += new System.EventHandler(this.btnGiris_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(54, 222);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Şifre Tekrar:";
+            // 
+            // txtSifreTekrar
+            // 
+            this.txtSifreTekrar.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSifreTekrar.Location = new System.Drawing.Point(49, 249);
+            this.txtSifreTekrar.Multiline = true;
+            this.txtSifreTekrar.Name = "txtSifreTekrar";
+            this.txtSifreTekrar.Size = new System.Drawing.Size(332, 26);
+            this.txtSifreTekrar.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(437, 409);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSifreTekrar);
+            this.Controls.Add(this.btnGiris);
+            this.Controls.Add(this.lblKullanici);
+            this.Controls.Add(this.txtKullanici);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblZorluk);
+            this.Controls.Add(this.lblYazi);
+            this.Controls.Add(this.txtSifre);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.Label lblYazi;
+        private System.Windows.Forms.Label lblZorluk;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblKullanici;
+        private System.Windows.Forms.TextBox txtKullanici;
+        private System.Windows.Forms.Button btnGiris;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSifreTekrar;
+    }
+}
+
---
+++ SifreZorlukDerecesi/SifreZorlukDerecesi/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SifreZorlukDerecesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtSifre_TextChanged(object sender, EventArgs e)
+        {
+            string ozelKarakterler = "\"'^_=)({}[].*?+#&!é/-\\";
+            bool ozelKarakter = false;
+            bool harf = false;
+            bool sayi = false;
+
+
+            foreach (char item in txtSifre.Text)
+            {
+                if (char.IsLetter(item))
+                {
+                    harf = true;
+                }
+                else if (char.IsDigit(item))
+                {
+                    sayi = true;
+                }
+                else if (ozelKarakterler.Contains(item))
+                {
+                    ozelKarakter = true;
+                }
+
+            }
+
+            if (txtSifre.TextLength < 6)
+            {
+                lblZorluk.ForeColor = Color.Red;
+                lblZorluk.Text = "Düşük";
+            }
+
+            else if (ozelKarakter == true && harf == true && sayi == true)
+            {
+                lblZorluk.ForeColor = Color.Green;
+                lblZorluk.Text = "Yüksek";
+            }
+
+            else if ((sayi == true && ozelKarakter == true) || (harf == true && ozelKarakter == true) || (sayi == true && harf == true))
+            {
+                lblZorluk.ForeColor = Color.Blue;
+                lblZorluk.Text = "Orta";
+            }
+
+            else
+            {
+                lblZorluk.ForeColor = Color.Red;
+                lblZorluk.Text = "Düşük";
+            }
+        }
+
+        private void btnGiris_Click(object sender, EventArgs e)
+        {
+            if (txtSifre.Text == txtSifreTekrar.Text)
+            {
+                MessageBox.Show("Hesabınız oluşturuldu!");
+                
+            }
+        }
+    }
+}
