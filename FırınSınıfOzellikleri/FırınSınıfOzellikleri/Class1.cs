d34c5900a0b0bc99ef7cbac36ca2527d9b11a8f9
Oguz Kagan Unal <107484578+kaganunal@users.noreply.github.com>
2023-02-16 09:10:59 +0300

    Fırın Sınıfları

---
+++ FırınSınıfOzellikleri/FırınSınıfOzellikleri/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FırınSınıfOzellikleri
+{
+    internal class Firin
+    {
+        public string Marka { get; set; }
+        public decimal Fiyat { get; set; }
+        public double Guc { get; set; }
+        public string Model { get; set; }
+        public int GarantiSuresi { get; set; }
+        public DateTime GarantiTarihi { get; set; }
+        public bool GarantiVarMi { get; set; }
+        public double MaxDerecesi { get; set; }
+
+        public Firin()
+        {
+            Console.WriteLine("Fırın oluşturuldu!");
+        }
+        public void GarantiUzat()
+        {
+            GarantiTarihi = GarantiTarihi.AddYears(3);
+        }
+
+        public void BilgiYazdir()
+        {
+
+            Console.WriteLine("Marka: " + Marka);
+            Console.WriteLine("Fiyat: " + Fiyat + "₺") ;
+            Console.WriteLine("Güç: " + Guc + "W");
+            Console.WriteLine("Model: " + Model);
+            Console.WriteLine("Garanti süresi: " + GarantiSuresi + "yıl");
+            Console.WriteLine("Garanti tarihi: " + GarantiTarihi.ToShortDateString());
+            Console.WriteLine("Garantisi var mı: " + GarantiVarMi);
+            Console.WriteLine("Max derecesi: " + MaxDerecesi + "°C");
+        }
+    }
+}
---
+++ FırınSınıfOzellikleri/FırınSınıfOzellikleri/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FırınSınıfOzellikleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Firin firin1 = new Firin();
+
+            firin1.Marka = "Bosch";
+            firin1.Fiyat = 13999m;
+            firin1.Guc = 1500;
+            firin1.Model = "HBF514BB0T";
+            firin1.GarantiSuresi = 2;
+            firin1.GarantiTarihi = Convert.ToDateTime("15.02.2025");
+            firin1.GarantiVarMi = true;
+            firin1.MaxDerecesi = 260;
+
+            firin1.GarantiUzat();
+
+            firin1.BilgiYazdir();
+
+            Console.WriteLine();
+
+            Firin firin2 = new Firin();
+
+            firin2.Marka = "Arçelik";
+            firin2.Fiyat = 10999m;
+            firin2.Guc = 900;
+            firin2.Model = "MF 30 QBB";
+            firin1.GarantiSuresi = 3;
+            firin2.GarantiTarihi = Convert.ToDateTime("15.02.2026");
+            firin2.GarantiVarMi = true;
+            firin2.MaxDerecesi = 240;
+
+
+
+            firin2.BilgiYazdir();
+
+            Console.ReadKey();
+        }
+    }
+}
