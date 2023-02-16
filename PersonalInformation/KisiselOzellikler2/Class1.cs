8c7f6675c214ddab88ea59ae453e61208d19a100
Oguz Kagan Unal <107484578+kaganunal@users.noreply.github.com>
2023-02-16 09:10:13 +0300

    CV and Personal Information

---
+++ PersonalInformation/KisiselOzellikler2/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KisiselOzellikler2
+{
+    public class Kagan
+    {
+        public static void Topla()
+        {
+
+        }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public DateTime DogumTarihi { get; set; }
+        public double Kilo { get; set; }
+        public int Boy { get; set; }
+        public short Yas { get; set; }
+        public List<string> BildigiDiller { get; set; }
+        public string MedeniHal { get; set; }
+        public string EMail { get; set; }
+        public string EhliyetTipi { get; set; }
+        public string Meslek { get; set; }
+        public List<string> BildigiProgramlar { get; set; }
+        public bool TamirYapabiliyorMu { get; set; }
+        public List<string> AkademikGecmis { get; set; }
+
+        public Kagan()
+        {
+            Console.WriteLine("Ben bugün OOP'ye başladım.");
+        }
+
+        public void GetKisiselBilgiler()
+        {
+
+            string yeniTarih = DogumTarihi.ToShortDateString();
+            string kisi = "\nBenim adım " + Ad + ", soyadım " + Soyad + ". " + yeniTarih + " tarihinde doğdum ve yaşım " + Yas + ". " + "Kilom " + Kilo + ", boyum " + Boy + " cm. Ehliyet tipim " + EhliyetTipi + " ve şu an BilgeAdam kurumunda " + Meslek + " olarak görev yapıyorum.";
+
+            Console.WriteLine(kisi);
+            Console.WriteLine("\nBildiği Diller: ");
+            foreach (var item in BildigiDiller)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("\nBildiği Programlama Dilleri ve Programlar: ");
+            foreach (var item in BildigiProgramlar)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("\nAkademik Geçmiş: ");
+            foreach (var item in AkademikGecmis)
+            {
+                Console.WriteLine(item + "\n");
+            }
+
+        }
+
+    }
+}
---
+++ PersonalInformation/KisiselOzellikler2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KisiselOzellikler2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Kagan oguzKagan = new Kagan
+            {
+                Ad = "Oguz Kagan",
+                Soyad = "Unal",
+                DogumTarihi = Convert.ToDateTime("21.10.1998"),
+                Yas = 24,
+                Kilo = 73.6,
+                Boy = 183,
+                BildigiDiller = new List<string> { "İngilizce B2", "Almanca A2" },
+                MedeniHal = "Bekar",
+                EMail = "oguzkaganu@gmail.com",
+                EhliyetTipi = "B",
+                Meslek = "Yıldızımsı Yazılımcı",
+                BildigiProgramlar = new List<string> { "C# (Advanced)", "Microsoft SQL Server (Advanced)", "T-SQL (Advanced)", "HTML", "Cascading Style Sheets (CSS)", "JavaScript", "ASP.NET Core", "ASP.NET Web API", "AutoCAD (Advanced)", "Revit (Beginner)", "Primavera P6 (Beginner)", "Slide 2 (Beginner)", "MS Project(Beginner)", "MS Office (Advanced)" },
+                TamirYapabiliyorMu = true,
+                AkademikGecmis = new List<string> { "Hacettepe University, Ankara/ Turkey, August2016 - September2021\r\nB.Sc. | Engineering Department | Civil Engineering\r\nEnglish (2.89 GPA)", "Middle East Technical University, Ankara/Turkey, February 2022 - Ongoing\r\nM.Sc. | Graduate School of Natural and Applied Sciences | Civil Engineering\r\nConstruction Materials Division (3.67 GPA)" }
+            };
+
+            oguzKagan.GetKisiselBilgiler();
+
+            Kagan.Topla();
+
+            Console.ReadKey();
+
+
+        }
+
+    }
+}
