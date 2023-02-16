c7918881005e780f678eec3cd0a462038c10b477
Oguz Kagan Unal <107484578+kaganunal@users.noreply.github.com>
2023-02-16 18:37:57 +0300

    Student Information Classes

---
+++ OgrenciBilgileriClass/OgrenciBilgileriClass/Ogrenci.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgrenciBilgileriClass
+{
+    internal class Ogrenci
+    {
+        private string _adSoyad;
+        private string _tcKimlikNo;
+        private int _ogrenciNo;
+
+        public string AdSoyad
+        {
+            get
+            {
+                return _adSoyad;
+            }
+
+            set
+            {
+                _adSoyad = value;
+            }
+        }
+
+        public string TCKimlikNo
+        {
+            get
+            {
+                return _tcKimlikNo;
+            }
+            set
+            {
+                _tcKimlikNo = value;
+            }
+        }
+
+        public int OgrenciNo
+        {
+            get
+            {
+                return _ogrenciNo;
+            }
+            set
+            {
+                _ogrenciNo = value;
+            }
+        }
+
+        public bool TCNoKontrol()
+        {
+            if (_tcKimlikNo.Length == 11)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool OgrenciNoKontrol()
+        {
+            if (_ogrenciNo.ToString().Length == 3)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+
+    }
+}
---
+++ OgrenciBilgileriClass/OgrenciBilgileriClass/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgrenciBilgileriClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ogrenci = new Ogrenci();
+
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+
+            while (true)
+            {
+                Console.WriteLine("Öğrenci Ad Soyad: ");
+                ogrenci.AdSoyad = textInfo.ToTitleCase(Console.ReadLine().Trim());
+            TCKimlikNo:
+
+                Console.WriteLine("TC Kimlik No giriniz: ");
+                ogrenci.TCKimlikNo = Console.ReadLine();
+                if (ogrenci.TCNoKontrol() != true)
+                {
+                    Console.WriteLine("TC Kimlik Numarası 11 haneli olmalıdır!");
+                    goto TCKimlikNo;
+                }
+                else
+                {
+                OgrenciNo:
+                    Console.WriteLine("Öğrenci numarasını giriniz: ");
+                    ogrenci.OgrenciNo = int.Parse(Console.ReadLine());
+                    if (ogrenci.OgrenciNoKontrol() != true)
+                    {
+                        Console.WriteLine("Öğrenci Numarası 3 basamaklı olmalıdır!");
+                        goto OgrenciNo;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Öğrencinin adı soyadı: {ogrenci.AdSoyad} \nTC Kimlik No: {ogrenci.TCKimlikNo}\nÖğrenci Numarası: {ogrenci.OgrenciNo}");
+                    }
+                }
+
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
