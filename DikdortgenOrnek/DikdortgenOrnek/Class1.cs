2850305a8151b714f1e5d03b8a0f933bde47bc25
Oguz Kagan Unal <107484578+kaganunal@users.noreply.github.com>
2023-02-16 18:36:49 +0300

    Dikdortgen çizimi için class oluşturuldu.

---
+++ DikdortgenOrnek/DikdortgenOrnek/Class1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DikdortgenOrnek
+{
+    internal class Dikdortgen
+    {
+        private string Name { get; set; }
+        private double SolX { get; set; }
+        private double SolY { get; set; }
+        private double SagX { get; set; }
+        private double SagY { get; set; }
+
+        public Dikdortgen(string name, double a, double b, double c, double d)
+        {
+            Name = name;
+            SolX = a;
+            SolY = b;
+            SagX = c;
+            SagY = d;
+        }
+
+        public void Yaz()
+        {
+            Console.WriteLine($"Adı: {Name}, Sol Üst: ({SolX}, {SolY}) Sağ Alt: ({SagX}, {SagY})");
+        }
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+        public void SetSolUst(double a, double b)
+        {
+            SolX= a;
+            SolY= b;
+
+        }
+        public void SetSagAlt(double a, double b)
+        {
+            SagX= a;
+            SagY= b;    
+        }
+
+        public double GetBoy()
+        {
+            return Math.Abs(SolY - SagY);
+        }
+
+        public double GetEn()
+        {
+            return Math.Abs(SagX - SagY);
+        }
+
+        public double GetAlan()
+        {
+            return GetEn() * GetBoy();
+        }
+
+        public double GetCevre()
+        {
+            return 2 * (GetEn() + GetBoy());
+        }
+
+        public Dikdortgen BuyukOlan(Dikdortgen other)
+        {
+            return this.GetAlan() > other.GetAlan() ? this : other;
+        }
+
+        public void KareYap()
+        {
+            if (GetEn() > GetBoy())
+            {
+                SagX = SolX + GetBoy();
+            }
+            else
+            {
+                SagY = SolY - GetEn();
+            }
+        }
+    }
+}
---
+++ DikdortgenOrnek/DikdortgenOrnek/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DikdortgenOrnek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdortgen d1 = new Dikdortgen("Dikdörtgen 1", 0, 15, 10, 0);
+            Dikdortgen d2 = new Dikdortgen("Dikdörtgen 2", 0, 8, 12, 0);
+
+            Dikdortgen buyukOlan = d1.BuyukOlan(d2);
+            buyukOlan.Yaz();
+            d1.BuyukOlan(d2).Yaz();
+            d2.BuyukOlan(d1).Yaz();
+
+            Dikdortgen d3 = new Dikdortgen("Dikdörtgen 3", 0, 8, 12, 0);
+            d1.Yaz();
+            d2.Yaz();
+            d3.Yaz();
+            d1.KareYap();
+            d2.KareYap();
+            d3.KareYap();
+            d1.Yaz();
+            d2.Yaz();
+            d3.Yaz();
+        }
+    }
+}
